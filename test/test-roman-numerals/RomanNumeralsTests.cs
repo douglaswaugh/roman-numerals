3fc3a3f499d19c8ebf5cc5dca4243cde1467f085
Douglas Waugh <douglas.waugh@energyhelpline.com>
2016-10-19 14:50:46 +0100

    Convert 4 to IV

--- test/test-roman-numerals/RomanNumeralsTests.cs
+++ test/test-roman-numerals/RomanNumeralsTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace RomanNumerals
@@ -8,6 +9,7 @@ namespace RomanNumerals
         [TestCase(0, "")]
         [TestCase(1, "I")]
         [TestCase(2, "II")]
+        [TestCase(4, "IV")]
         public void Should_return_numerals_for_number(int number, string expectedNumerals)
         {
             var numerals = GetNumeralsFor(number);
@@ -15,17 +17,27 @@ namespace RomanNumerals
             Assert.That(numerals, Is.EqualTo(expectedNumerals));
         }
 
+        // put the right number of numerals in 
+        // pass on the remainder
+
+        // something to do with the base ten-ness; multiples of 1's, 10's, 100's, etc. can exist, but not the other numbers
+
         public string GetNumeralsFor(int numberToConvert)
         {
-            if (numberToConvert > 0) {
-                var numerals = string.Empty;
+            if (numberToConvert == 0)
+                return string.Empty;
+
+            var numerals = string.Empty;
+            if (numberToConvert / 4 >= 1) {
+                var numberOfFives = Math.Floor(((decimal)numberToConvert / 4));
+                numerals = "IV";
+            }
+            else if (numberToConvert <=3) {
                 for (var i = numberToConvert; i > 0; i--) {
                     numerals += "I";
                 }
-                return numerals;
             }
-
-            return string.Empty;
+            return numerals;
         }
     }
 }
