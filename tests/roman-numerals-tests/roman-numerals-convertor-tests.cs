c238514acc2b026dfa89308088399794ff1bc409
Douglas Waugh <douglas.waugh.e3@gmail.com>
2017-04-17 12:53:49 +0100

    Add tests for combinations of X, V and I

--- tests/roman-numerals-tests/roman-numerals-convertor-tests.cs
+++ tests/roman-numerals-tests/roman-numerals-convertor-tests.cs
@@ -11,6 +11,8 @@ namespace RomanNumeralsTests
     [TestCase(4, "IV")]
     [TestCase(5, "V")]
     [TestCase(10, "X")]
+    [TestCase(15, "XV")]
+    [TestCase(18, "XVIII")]
     public void Should_return_roman_numeral_for_arabic(int arabic, string expectedRomanNumeral)
     {
       var romanNumeralsConvertor = new RomanNumeralsConvertor();
